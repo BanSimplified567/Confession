a184c58d21cc29b95a69cce5e325d018ac510f9a
Blacklion567 <naviedaj567@gmail.com>
2024-07-23 08:35:52 +0800

    Suki desu

---
+++ config.js
@@ -0,0 +1,20 @@
+const CONFIG = {
+   titleWeb: 'SUKI DESU 🤍⁉️',
+   introTitle: 'Can I Confess My Love To U? 💌⁉️',
+   introDesc: `This will be a quick confession.
+    I just want to know what you feel about me,
+    all you have to do is follow my heart charotttt HEHEHE.
+    Follow the instructions carefully bitaw naa ra nmu heheh.😝😍`,
+   btnIntro: `😚 HEHE 😍`,
+   title: 'You know, I’ve had a crush on you ever since I saw you. Maybe this is what they call love at first sight 😄',
+   desc: 'Please Choose wisely 😚😱',
+   btnYes: 'YES!!!! 😄',
+   btnNo: 'NO??? 😱',
+   question: `What do you like about me? I don't have talents, looks, skills, or communication skills, I only have my pure and loyal heart . 💖😱`,
+   btnReply: 'I Love You.',
+   reply: `I Love The Way You Are...HAHAHA🤣🤣🤣`,
+   mess: `I was just playing around you know how much I love you right hehe 😚 eyyyy🤙 eyyyy🤙 dayun🤭💖 `,
+   messDesc: 'Thank You For The Participation HHEHE Luv U 😝⁉❗️ ',
+   btnAccept: 'Love V too 😽🥰️⁉️',
+   messLink: 'https://web.facebook.com/him.blacklion567/',
+};
---
+++ js/main.js
@@ -0,0 +1,179 @@
+$(document).ready(function () {
+   // process bar
+   setTimeout(function () {
+      firstQuestion();
+      $('.spinner').fadeOut();
+      $('#preloader').delay(350).fadeOut('slow');
+      $('body').delay(350).css({
+         overflow: 'visible',
+      });
+   }, 600);
+});
+
+let audio = new Audio('sound/Sukidesu.mp3');
+audio.play();
+
+function init() {
+   document.getElementById('titleWeb').innerHTML = CONFIG.titleWeb;
+   $('#title').text(CONFIG.title);
+   $('#desc').text(CONFIG.desc);
+   $('#yes').text(CONFIG.btnYes);
+   $('#no').text(CONFIG.btnNo);
+
+   var xYes = (0.9 * $(window).width() - $('#yes').width() - $('#no').width()) / 2;
+   var xNo = xYes + $('#yes').width() + 0.1 * $(window).width();
+   var y = 0.75 * $(window).height();
+   $('#yes').css('left', xYes);
+   $('#yes').css('top', y);
+
+   $('#no').css('left', xNo);
+   $('#no').css('top', y);
+}
+
+function firstQuestion() {
+   $('.content').hide();
+   Swal.fire({
+      title: CONFIG.introTitle,
+      text: CONFIG.introDesc,
+      imageUrl: 'img/logi.gif',
+      imageWidth: 300,
+      imageHeight: 300,
+      background: '#fff url("img/iput-bg.jpg")',
+      imageAlt: 'Custom image',
+      confirmButtonText: CONFIG.btnIntro,
+   }).then(function () {
+      $('.content').show(200);
+   });
+}
+
+// switch button position
+function switchButton() {
+   var audio = new Audio('sound/duck.mp3');
+   audio.play();
+   var leftNo = $('#no').css('left');
+   var topNO = $('#no').css('top');
+   var leftY = $('#yes').css('left');
+   var topY = $('#yes').css('top');
+   $('#no').css('left', leftY);
+   $('#no').css('top', topY);
+   $('#yes').css('left', leftNo);
+   $('#yes').css('top', topNO);
+}
+
+// move random button position
+function moveButton() {
+   var x = Math.random() * ($(window).width() - $('#no').width()) * 0.9;
+   var y = Math.random() * ($(window).height() - $('#no').height()) * 0.9;
+   var left = x + 'px';
+   var top = y + 'px';
+   $('#no').css('left', left);
+   $('#no').css('top', top);
+}
+
+init();
+
+var n = 0;
+$('#no').mousemove(function () {
+   if (Math.random() < 0.5 || n == 1) switchButton();
+   else moveButton();
+   n++;
+});
+
+$('#no').click(() => {
+   if (screen.width >= 900) switchButton();
+});
+
+// generate text in input
+function textGenerate() {
+   var n = '';
+   var text = ' ' + CONFIG.reply;
+   var a = Array.from(text);
+   var textVal = $('#txtReason').val() ? $('#txtReason').val() : '';
+   var count = textVal.length;
+   if (count > 0) {
+      for (let i = 1; i <= count; i++) {
+         n = n + a[i];
+         if (i == text.length + 1) {
+            $('#txtReason').val('');
+            n = '';
+            break;
+         }
+      }
+   }
+   $('#txtReason').val(n);
+   setTimeout('textGenerate()', 1);
+}
+
+// Function to check input and trigger custom alert if conditions are met
+function checkInputAndShowPopup(input) {
+   const name = input.toLowerCase();
+   let message = "You're the one that I like!";
+
+   if (
+      name.startsWith('c') ||
+      name == 'cyramae labis' ||
+      name == 'cy cy' ||
+      name == 'cycy' ||
+      name == 'cycylabis'
+   ) {
+      message = "You're the one that I like, Cyramae Labis!";
+   } else if (
+      name.startsWith('s') ||
+      name === 'shanley remolado' ||
+      name == 'shanleyremolado' ||
+      name == 'shanley' ||
+      name == 'shan' ||
+      name == 'remolado' ||
+      name == 'shizu'
+   ) {
+      message = "You're the one that I like, Shanley Remolado!";
+   } else if (name.startsWith('j') || name === 'jade m lee') {
+      message = "You're the one that I like, Jade M Lee!";
+   } else if (name === 'vanese lim') {
+      message = "You're the one that I like, Vanese Lim!";
+   }
+
+   $('#customAlert').text(message).fadeIn();
+
+   setTimeout(() => {
+      $('#customAlert').fadeOut();
+   }, 3000);
+}
+
+// show popup
+$('#yes').click(function () {
+   const userInput = prompt('Enter your name:'); // Replace with actual input method
+   checkInputAndShowPopup(userInput);
+
+   Swal.fire({
+      title: CONFIG.question,
+      html: true,
+      width: 900,
+      padding: '3em',
+      html: "<input type='text' class='form-control' id='txtReason' onmousemove=textGenerate() placeholder='Whyyy thou????'>",
+      background: '#fff url("img/iput-bg.jpg")',
+      backdrop: `
+              rgba(0,0,123,0.4)
+              url("img/giphy2.gif")
+              left top
+              no-repeat
+            `,
+      confirmButtonColor: '#3085d6',
+      confirmButtonColor: '#fe8a71',
+      confirmButtonText: CONFIG.btnReply,
+   }).then((result) => {
+      if (result.value) {
+         Swal.fire({
+            width: 900,
+            confirmButtonText: CONFIG.btnAccept,
+            background: '#fff url("img/iput-bg.jpg")',
+            title: CONFIG.mess,
+            text: CONFIG.messDesc,
+            confirmButtonColor: '#83d0c9',
+            onClose: () => {
+               window.location = CONFIG.messLink;
+            },
+         });
+      }
+   });
+});
